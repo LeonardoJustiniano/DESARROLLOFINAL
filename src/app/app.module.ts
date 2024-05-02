d6cad46ac582a5fe8ec882e36dc6a020443100fe
LeonardoJustiniano <166728667+LeonardoJustiniano@users.noreply.github.com>
2024-05-02 10:38:47 -0400

    Practica2

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroesComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule // Add FormsModule to imports
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+
+})
+export class AppModule { }
+
